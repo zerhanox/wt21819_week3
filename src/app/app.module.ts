bb3b401a2dea6035af5e7f2e789a250c4e2dcb85
zerhanox <rodomaxmilano@gmail.com>
2019-03-18 20:54:05 +0700

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { StudentComponent } from "./student/student.component";
 import { StudentDetailComponent } from "./student-detail/student-detail.component";
